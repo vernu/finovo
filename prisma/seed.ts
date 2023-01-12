8882d5fd50770c170eccd0c5b92a00e9dc64f6d5
isra el <vernu1997@gmail.com>
2023-01-12 21:52:33 +0300

    add seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,65 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const seedCurrencies = async () => {
+  const currencies = [
+    {
+      name: 'US Dollar',
+      code: 'USD',
+      symbol: '$',
+    },
+    {
+      name: 'Birr',
+      code: 'ETB',
+      symbol: 'Br',
+    },
+    {
+      name: 'Euro',
+      code: 'EUR',
+      symbol: '€',
+    },
+    {
+      name: 'GB Pound',
+      code: 'GBP',
+      symbol: '£',
+    },
+    {
+      name: 'Canadian Dollar',
+      code: 'CAD',
+      symbol: 'C$',
+    },
+    {
+      name: 'Cryptocurrency (USD Equivalent)',
+      code: 'CRYPTO',
+      symbol: '₿',
+    },
+  ]
+
+  for (let currency of currencies) {
+    await prisma.currency.upsert({
+      where: { code: currency.code },
+      update: {
+        name: currency.name,
+        symbol: currency.symbol,
+      },
+      create: {
+        name: currency.name,
+        code: currency.code,
+        symbol: currency.symbol,
+      },
+    })
+  }
+}
+
+async function main() {
+  await seedCurrencies()
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
