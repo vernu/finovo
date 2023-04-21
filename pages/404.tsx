906b88f4f66eb59697fd96da6c1b10efd20780e1
isra el <vernu1997@gmail.com>
2023-04-21 14:36:03 +0300

    custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import { Grid, Typography } from '@mui/material'
+import { withDefaultLayout } from '../HOC/withDefaultLayout'
+
+const Custom404 = () => {
+  return (
+    <>
+      <Grid
+        container
+        spacing={0}
+        direction='column'
+        alignItems='center'
+        justifyContent='center'
+        style={{ minHeight: '90vh', background: '#f5f5f5' }}
+      >
+        <Grid item xs={12} sm={6} md={4}>
+          <Typography variant='h4' component='h1' gutterBottom>
+            404 - Page Not Found
+          </Typography>
+          <Typography variant='body1' component='p'>
+            The page you are looking for does not exist.
+          </Typography>
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export default withDefaultLayout(Custom404)
