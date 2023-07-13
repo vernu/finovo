b8fa2ed89ebd96b2ed62181b1198d4e83444dbee
isra el <vernu1997@gmail.com>
2023-07-13 19:17:54 +0300

    chore: sort categories by type

--- src/server/resolvers/category.ts
+++ src/server/resolvers/category.ts
@@ -5,8 +5,21 @@ export const categoryListResolver = async (
   _args: any,
   ctx: Context
 ) => {
-  const categories = await ctx.prisma.category.findMany({})
-  return categories
+  let categories = await ctx.prisma.category.findMany({})
+
+  const sortOrder: any = {
+    INCOME: 1,
+    EXCHANGE: 2,
+    EXPENSE: 3,
+  }
+
+  const sortedCategories = categories.sort((a, b) => {
+    const aOrder = a.type != null ? sortOrder[a.type] : 4
+    const bOrder = b.type != null ? sortOrder[b.type] : 4
+    return aOrder - bOrder
+  })
+
+  return sortedCategories
 }
 
 export const singleCategoryResolver = async (
