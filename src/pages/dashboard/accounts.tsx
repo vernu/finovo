e1c5240989a38b84b2548b8fe35176aef30596d3
isra el <vernu1997@gmail.com>
2023-07-17 04:26:04 +0300

    chore: update dashboard sidenav

--- src/pages/dashboard/accounts.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import withDashboardLayout from '../../HOC/withDashboardLayout'
-
-const Accounts = () => {
-  return <div>accounts</div>
-}
-
-export default withDashboardLayout(Accounts)
