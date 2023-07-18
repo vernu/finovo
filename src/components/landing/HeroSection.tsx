08e7618a47106c4f70c29286836d960de0c87695
isra el <vernu1997@gmail.com>
2023-07-18 07:18:59 +0300

    ui: improve landing page ui

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -1,9 +1,10 @@
-import { Mediation, SettingsOverscan, Token } from '@mui/icons-material'
+import { Login, Mediation, SettingsOverscan, Token } from '@mui/icons-material'
 import { Box, Button, Typography } from '@mui/material'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import Icon from '@mui/material/Icon'
 import Stack from '@mui/material/Stack'
+import Image from 'next/image'
 import Link from 'next/link'
 import { HiArrowSmRight } from 'react-icons/hi'
 
@@ -11,29 +12,64 @@ function HeroSection() {
   return (
     <Box component='section' py={{ xs: 4, md: 16 }}>
       <Container>
-        <Grid container alignItems='center'>
-          <Grid item xs={12} lg={5}>
-            <Typography variant='h3' my={1} color='gray'>
-              Manage your personal finance with ease
-            </Typography>
-            <Typography variant='body2' color='text' mb={2} textAlign='justify'>
-              Budget, track and manage your finances with ease in one place.
-              With our app, you can manage your finances in one place. You can
-              also import and export your data with ease.
-            </Typography>
-            <Button
-              component={Link}
-              href='/login'
-              variant='contained'
-              color='primary'
-              sx={{
-                borderRadius: 999,
-                textTransform: 'none',
-              }}
-              endIcon={<HiArrowSmRight size={16} />}
-            >
-              Sign up now!
-            </Button>
+        <Grid container alignItems='center' columnSpacing={12}>
+          <Grid item xs={12} lg={6}>
+            <Grid container spacing={1}>
+              <Grid item xs={5} md={4}>
+                <Image
+                  src='/illustration1.png'
+                  alt='Description of the image'
+                  width={150}
+                  height={150}
+                />
+              </Grid>
+              <Grid
+                item
+                xs={7}
+                md={8}
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+              >
+                {' '}
+                <Typography variant='h5' my={1}>
+                  Manage your personal finance with ease
+                </Typography>{' '}
+                <Typography
+                  variant='body2'
+                  color='gray'
+                  mb={2}
+                  textAlign='justify'
+                >
+                  Budget, track and manage your finances with ease in one place.
+                </Typography>
+                <Box>
+                  <Button
+                    component={Link}
+                    href='/login'
+                    variant='contained'
+                    color='primary'
+                    sx={{
+                      borderRadius: 999,
+                      textTransform: 'none',
+                    }}
+                    startIcon={<Login />}
+                  >
+                    Sign up now!
+                  </Button>
+                  {/* <Button
+                    variant='text'
+                    color='primary'
+                    sx={{
+                      borderRadius: 999,
+                      textTransform: 'none',
+                    }}
+                  >
+                    How it works!
+                  </Button> */}
+                </Box>
+              </Grid>
+            </Grid>
           </Grid>
           <Grid
             item
