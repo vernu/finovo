c031e7f1e627792fd38717e7e68a2c91e19e935f
isra el <vernu1997@gmail.com>
2023-07-26 02:56:20 +0300

    pg

--- prisma/migrations/20230717022804_add_emoji_description_and_active_fields_to_categories_table/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Category" ADD COLUMN     "active" BOOL NOT NULL DEFAULT true;
-ALTER TABLE "Category" ADD COLUMN     "description" STRING;
-ALTER TABLE "Category" ADD COLUMN     "emoji" STRING;
--- prisma/migrations/20230725160918_init/migration.sql
+++ prisma/migrations/20230725160918_init/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" STRING NOT NULL,
-    "name" STRING,
-    "email" STRING,
-    "phone" STRING,
-    "password" STRING,
+    "id" UUID NOT NULL,
+    "name" VARCHAR,
+    "email" VARCHAR,
+    "phone" VARCHAR,
+    "password" VARCHAR,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -13,10 +13,14 @@ CREATE TABLE "User" (
 
 -- CreateTable
 CREATE TABLE "Category" (
-    "id" STRING NOT NULL,
-    "name" STRING,
-    "slug" STRING,
-    "type" STRING,
+    "id" UUID NOT NULL,
+    "name" VARCHAR,
+    "slug" VARCHAR,
+    "type" VARCHAR(20),
+    "emoji" VARCHAR(20),
+    "description" TEXT,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "userId" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -25,10 +29,10 @@ CREATE TABLE "Category" (
 
 -- CreateTable
 CREATE TABLE "Currency" (
-    "id" STRING NOT NULL,
-    "name" STRING NOT NULL,
-    "code" STRING NOT NULL,
-    "symbol" STRING,
+    "id" UUID NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "code" VARCHAR(20) NOT NULL,
+    "symbol" VARCHAR(20),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -37,13 +41,13 @@ CREATE TABLE "Currency" (
 
 -- CreateTable
 CREATE TABLE "Transaction" (
-    "id" STRING NOT NULL,
-    "amount" FLOAT8 NOT NULL,
-    "currencyId" STRING,
+    "id" UUID NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "currencyId" UUID,
     "date" DATE DEFAULT CURRENT_TIMESTAMP,
-    "userId" STRING,
-    "categoryId" STRING,
-    "description" STRING,
+    "userId" UUID,
+    "categoryId" UUID,
+    "description" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -60,7 +64,40 @@ CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
 CREATE UNIQUE INDEX "Category_slug_key" ON "Category"("slug");
 
 -- CreateIndex
+CREATE INDEX "Category_userId_idx" ON "Category"("userId");
+
+-- CreateIndex
+CREATE INDEX "Category_slug_idx" ON "Category"("slug");
+
+-- CreateIndex
+CREATE INDEX "Category_userId_slug_idx" ON "Category"("userId", "slug");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Currency_code_key" ON "Currency"("code");
+
+-- CreateIndex
+CREATE INDEX "Currency_code_idx" ON "Currency"("code");
+
+-- CreateIndex
+CREATE INDEX "Transaction_currencyId_idx" ON "Transaction"("currencyId");
+
+-- CreateIndex
+CREATE INDEX "Transaction_userId_idx" ON "Transaction"("userId");
+
+-- CreateIndex
+CREATE INDEX "Transaction_categoryId_idx" ON "Transaction"("categoryId");
+
+-- CreateIndex
+CREATE INDEX "Transaction_date_idx" ON "Transaction"("date");
+
+-- CreateIndex
+CREATE INDEX "Transaction_description_idx" ON "Transaction"("description");
+
+-- CreateIndex
+CREATE INDEX "Transaction_currencyId_userId_categoryId_date_description_idx" ON "Transaction"("currencyId", "userId", "categoryId", "date", "description");
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_currencyId_fkey" FOREIGN KEY ("currencyId") REFERENCES "Currency"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
